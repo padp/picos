cdf1758382ea96867c8047ad29a40598ad07c321
sol-vend <solanavending@tutamail.com>
2025-01-07 14:41:11 -0600

    Update api-url

--- src/Constants.js
+++ src/Constants.js
@@ -1,2 +1,2 @@
-export const API_URI = "https://bridge-api-production-81ea.up.railway.app/data" 
+export const API_URI = "https://bridge-api-production-e866.up.railway.app/data" 
 export const DEFAULT_PAUSE_TIME = 5000;
